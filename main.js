6a1ad4c0d5cd0c14714089e4d7fbe2997affed8e
squalle0nhart <20115754.bka@gmail.com>
2018-05-31 09:26:27 +0700

    Update README

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 let errorWindow
 
@@ -19,41 +17,29 @@ function createWindow () {
       plugins: true  // set it explicitly to false even if it should be the default
     }
   })
-  mainWindow.loadURL('file:///pdf-doc-vba-eicar-dropper.pdf')
-  mainWindow.webContents.openDevTools()
+
+  // password for this test file is: 123456
+  mainWindow.loadURL('file:///pdf-sample.protected.pdf')
+  // mainWindow.webContents.openDevTools()
   mainWindow.on('closed', function () {
     mainWindow = null
   })
 
-  // errorWindow = new BrowserWindow({width: 800, height: 600})
-  // errorWindow.loadURL('https://1drv.ms/b/s!AtilKnwD6Yt6xSqGHCpSLlZF0ZUz')
-  // errorWindow.webContents.openDevTools()
-  // errorWindow.on('closed', function () {
-  //   errorWindow = null
-  // })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
+
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
